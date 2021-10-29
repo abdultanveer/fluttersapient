9b347f7a09d744e271ba39983c05d0894c218b70
abdul <abdul.tanveer@gmail.com>
2021-10-29 09:18:54 +0530

    scaffold and appbar properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text('welcome to flutter'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('appbar title'),
+          centerTitle: true,
+          backgroundColor: Colors.green[500],
+        ),
+      ),
     ));
